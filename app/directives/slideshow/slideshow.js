dd9122a43d73132d73958091f65b9cfa61bbc584
Elliott Sencan <elliottsencan88@gmail.com>
2014-09-03 00:38:28 -0700

    added a bunch of styles cleanup. NOt finalized, but were getting there. loading animations are cleaned up, and added in the multithumbnail hover over arrows to select the next or previous tab

--- app/directives/slideshow/slideshow.js
+++ app/directives/slideshow/slideshow.js
@@ -4,8 +4,8 @@
     angular
         .module('instatest.components', ['instatest.api'])
         .directive('slideshow', Slideshow)
-        ///move this out of here asap
-        .directive('imageloader', ImageOnload);
+    ///move this out of here asap
+    .directive('imageloader', ImageOnload);
 
     Slideshow.$inject = ['instagramEndpoint', '$timeout', '$rootScope'];
 
@@ -18,16 +18,36 @@
                 ngModel: '='
             },
             template: [
-                '<div class="slider">',
-                '<div class="slide" ng-repeat="image in images">',
-                '<img imageloader ng-src="{{image.images.standard_resolution.url}}" />',
-                '</div>',
-                '<div class="arrows">',
-                '<a href="#" ng-click="prev()">',
-                '</a>',
-                '<a href="#" ng-click="next()">',
-                '</a>',
-                '</div>',
+                '<div class="slider centered">',
+                	'<div class="slide" ng-repeat="image in images" ng-show="image.visible">',
+                		'<img imageloader ng-src="{{image.images.standard_resolution.url}}" />',
+                	'</div>',
+                	'<div class="arrows">',
+                		'<a class="prev" ng-click="prev()">',
+			            	'<span class="icon-wrap">',
+			            		'<svg class="icon" width="32" height="32" viewBox="0 0 64 64">',
+			            			'<use xlink:href="#arrow-left-3"></use>',
+			            		'</svg>',
+			            	'</span>',
+			            	'<div>',
+			            		'<img ng-src="{{image.images.standard_resolution.url}}" alt="Thumb 1">',
+			            		'<img ng-src="{{image.images.thumbnail.url}}" alt="Thumb 2">',
+			            		'<img ng-src="{{image.images.thumbnail.url}}" alt="Thumb 3">',
+			            	'</div>',
+			            '</a>',
+			            '<a class="next" ng-click="next()">',
+			            	'<span class="icon-wrap">',
+			            		'<svg class="icon" width="32" height="32" viewBox="0 0 64 64">',
+			            			'<use xlink:href="#arrow-right-3"></use>',
+			            		'</svg>',
+			            	'</span>',
+			            	'<div>',
+			            		'<img ng-src="{{image.images.thumbnail.url}}" alt="Thumb 1">',
+			            		'<img ng-src="{{image.images.thumbnail.url}}" alt="Thumb 2">',
+			            		'<img ng-src="{{image.images.thumbnail.url}}" alt="Thumb 3">',
+			            	'</div>',
+			            '</a>',
+                	'</div>',
                 '</div>'
             ].join(''),
             controller: SlideshowController,
@@ -44,6 +64,7 @@
                         console.log(data);
                         $rootScope.safeApply(function() {
                             $scope.images = data;
+                            $scope.images[0].visible= true;
                         });
                     },
                     function error(error) {
@@ -59,7 +80,6 @@
 
             scope.$watch('ngModel', function(newVal) {
                 if (newVal) {
-                	console.log(iElement);
                     scope.getImages(scope.ngModel);
                 }
             });
@@ -68,12 +88,11 @@
     }
 
     function ImageOnload() {
-    	console.log('yo');
         return {
             restrict: 'A',
             link: function(scope, element, attrs) {
                 element.bind('load', function() {
-                    console.log(element[0].classList.add('visible'));
+                    element[0].classList.add('visible');
                 });
             }
         };
